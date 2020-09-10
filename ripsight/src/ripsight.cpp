d5867ff1a3b4d4bbb9b3a0cde90c5d23c4f1f6bc
iozsaygi <devozsaygi@gmail.com>
2020-09-10 15:26:30 +0300

    Added temporary background

--- ripsight/src/ripsight.cpp
+++ ripsight/src/ripsight.cpp
@@ -9,11 +9,19 @@ int main(int argc, char* argv[])
 	// Create test world.
 	engine::World* world = new engine::World(engineEntry, true);
 
+	// Craft the background.
+	engine::Actor* background = new engine::Actor();
+	engine::Transform* backgroundTransform = new engine::Transform(background, engine::Vector2D::Zero(), engine::Vector2D(800, 600));
+	background->AddComponent(backgroundTransform);
+	engine::SpriteRenderer* backgroundRenderer = new engine::SpriteRenderer(background, "assets/imgs/entities/background/background.png");
+	background->AddComponent(backgroundRenderer);
+	world->SubscribeToRenderQueue(backgroundRenderer);
+
 	// Craft the player and add it to the test world.
 	engine::Actor* player = new engine::Actor();
 	engine::Transform* playerTransform = new engine::Transform(player, engine::Vector2D(300, 200), engine::Vector2D(48, 48));
 	player->AddComponent(playerTransform);
-	engine::SpriteRenderer* spriteRenderer = new engine::SpriteRenderer(player, "assets/imgs/entities/player/Player.png");
+	engine::SpriteRenderer* spriteRenderer = new engine::SpriteRenderer(player, "assets/imgs/entities/player/player.png");
 	PlayerController* playerController = new PlayerController(player, engine::Vector2D(3.0f, 3.0f));
 	player->AddComponent(playerController);
 
